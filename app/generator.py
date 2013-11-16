c30bf5d0ab61188dcb66ab16eb8b9e44d23172aa
Connor Peet <connor@connorpeet.com>
2013-11-16 16:26:10 -0600

    Generation backend init

--- app/generator.py
+++ app/generator.py
@@ -1,8 +1,25 @@
 from app import app
-import flask, flask.views
+import flask, flask.views, request
 
 class GeneratorView(flask.views.MethodView):
 	def get(self):
 		return flask.render_template('generate.html');
 
-app.add_url_rule('/create', view_func = GeneratorView.as_view('create'))
+	def post(self):
+
+		parts = []
+		output = ''
+
+		for name, value in request.form:
+			section, attribute = name.split('_', 1)
+			parts[section][attribute] = value
+
+		for section, attributes in parts:
+			output += '[' + section + ']'
+			for attribute, value in attributes:
+				output += '\n' + attribute + '=' + value
+
+		return output
+
+
+app.add_url_rule('/create', view_func = GeneratorView.as_view('create'), methods = ['GET', 'POST'])
