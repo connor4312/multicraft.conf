8ce4ee6a361e33d814d5ccff7e36dc6bf014259a
Connor Peet <connor@connorpeet.com>
2013-11-17 20:46:42 -0600

    Added correct titles

--- app/index.py
+++ app/index.py
@@ -6,12 +6,15 @@ from database import connection, models
 @app.route('/')
 def home():
 	entries = connection.session.query(models.Entry).all()
-	return flask.render_template('index.html', entries = entries);
+	return flask.render_template('index.html', entries = entries, title = 'All Configs');
 
 @app.route('/jar/<int:id>')
-def list_by_jar(jar_id):
-	return 'foo'
+def list_by_jar(id):
+	entries = connection.session.query(models.Entry).filter(models.Entry.jar_id == id).all()
+	jar = connection.session.query(models.Jar).filter(models.Jar.id == id).first()
+	return flask.render_template('index.html', entries = entries, title = 'Configs for ' + jar.name);
 
 @app.route('/version/<string:name>')
-def list_by_version(version_name):
-	return 'foo'
+def list_by_version(name):
+	entries = connection.session.query(models.Entry).filter(models.Entry.version == name).all()
+	return flask.render_template('index.html', entries = entries, title = 'Configs for version ' + name);
--- app/post.py
+++ app/post.py
@@ -1,6 +1,7 @@
 from app import app
 
 import flask
+from flask import Response
 
 from database import connection, models
 
@@ -16,3 +17,12 @@ def show_conf_file(entry_id):
 		data = inputf.read()
 
 	return flask.render_template('post.html', entry = entry, data = data);
+
+@app.route('/conf/<int:entry_id>/raw')
+def show_conf_file_raw(entry_id):
+
+	try:
+		with open('storage/post' + str(entry_id) + '.txt', 'r') as inputf:
+			return Response(inputf.read(), mimetype="text/plain")
+	except IOError:
+		print '404'
